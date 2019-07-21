500c4d21d888fc5e4b56e9fea72625134f7e69f8
rajabli7 <rajabli7@users.noreply.github.com>
2019-07-21 11:33:09 +0300

    myController
    add elseverController

---
+++ SIS Test/SIS Test/Controllers/ElseverController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SIS_Test.Controllers
+{
+    public class ElseverController : Controller
+    {
+        // controller
+        // my branch
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
